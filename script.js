bd73f41e80bc14249ff3c9f34ba0303847767197
Burness Scott III <bsiii102798@gmail.com>
2023-04-13 23:45:39 -0700

    starter

---
+++ script.js
@@ -0,0 +1,10 @@
+var start_btn = document.querySelector(".start_btn button");
+const start_btn = document.querySelector(".start_btn button");
+var Info_box = document.querySelector(".Info_box");
+const Info_box = document.querySelector(".Info_box");
+
+
+start_btn.onclick= () =>{
+    Info_box.classList.add("activeinfo")
+}
+console.log(start_btn)
