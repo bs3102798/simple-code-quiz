bdc536c6e4de0021b85d4578e30967867dfa3a8c
Burness Scott III <bsiii102798@gmail.com>
2023-04-14 18:12:36 -0700

    additions

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = document.querySelector(".buttons .restart");
+
+
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo");
+
+}
+
+exit_btn.onclick =()=>{
+    info_box.classList.remove("activeInfo");
+}
+
